a05690f4b658767f0332b4cbd6122d93437dc3b0
tejasai38409@gmail.com <tejasai38409@gmail.com>
2024-12-28 14:58:56 +0530

    ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -304,7 +304,7 @@ export default function IndexPage() {
           />
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label className="pl-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-white">
-              Upload Profile Picture
+              Upload Profile Picture if you want
             </Label>
             <Input
               id="picture"
